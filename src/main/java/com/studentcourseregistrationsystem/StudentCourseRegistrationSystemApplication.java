aa4c8d4fac340764fe359e45310934468dd5960f
AdityaKalambe01 <adityakalambe20@gmail.com>
2024-02-11 23:48:07 +0530

    first commit

---
+++ src/main/java/com/studentcourseregistrationsystem/StudentCourseRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.studentcourseregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentcourseregistrationsystem/StudentCourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentcourseregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
